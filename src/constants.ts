97adb25a22a988fa4b4b78beae1ddeaf7a6c7c39
radiium <remip.dev@gmail.com>
2023-12-25 16:40:24 +0100

    fix: app contants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
 export const packageName = 'svelte-stuff-kit'
-export const packageHomePage = 'httpx//www.github.com/radiium/svelte-stuff-kit'
+export const packageHomePage = 'https//www.github.com/radiium/svelte-stuff-kit'
