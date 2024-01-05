cfddece99709a201226b8f194c69db7068818216
radiium <remip.dev@gmail.com>
2024-01-05 21:52:57 +0100

    style indent code

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,22 +1,19 @@
-export const size0 = '0';
-export const size1 = '1';
-export const size2 = '2';
-export const size3 = '3';
-export const size4 = '4';
-export const size5 = '5';
-export const size6 = '6';
-export const size7 = '7';
-export const size8 = '8';
-export const size9 = '9';
+export const size0 = '0' as const;
+export const size1 = '1' as const;
+export const size2 = '2' as const;
+export const size3 = '3' as const;
+export const size4 = '4' as const;
+export const size5 = '5' as const;
+export const size6 = '6' as const;
+export const size7 = '7' as const;
+export const size8 = '8' as const;
+export const size9 = '9' as const;
 export const Sizes0To1 = [size0, size1] as const;
 export const Sizes1To3 = [size1, size2, size3] as const;
 export const Sizes1To4 = [size1, size2, size3, size4] as const;
 export const Sizes1To5 = [size1, size2, size3, size4, size5] as const;
 export const Sizes1To9 = [size1, size2, size3, size4, size5, size6, size7, size8, size9] as const;
 export const Sizes0To9 = [size0, size1, size2, size3, size4, size5, size6, size7, size8, size9] as const;
-
-export const AsElements = ['button', 'submit', 'reset'] as const;
-export const ButtonTypes = ['button', 'submit', 'reset'] as const;
 export const Colors = ['neutral', 'primary', 'info', 'success', 'warning', 'danger'] as const;
 export const Variants = ['solid', 'soft', 'outline', 'clear'] as const;
 export const Weights = ['light', 'regular', 'medium', 'bold'] as const;
@@ -31,17 +28,4 @@ export const Wraps = ['nowrap', 'wrap', 'wrap-reverse'] as const;
 export const Gaps = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] as const;
 export const Grows = ['0', '1'] as const;
 export const Shrinks = ['0', '1'] as const;
-export const InputTypes = [
-	'number',
-	'time',
-	'text',
-	'search',
-	'password',
-	'email',
-	'tel',
-	'url',
-	'date',
-	'datetime-local',
-	'month',
-	'week'
-] as const;
+export const InputTypes = ['number', 'time', 'text', 'search', 'password', 'email', 'tel', 'url', 'date',	'datetime-local', 'month', 'week'] as const;
