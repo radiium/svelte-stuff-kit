d76c2636ddf1a8ae630419cea48ed0050151101a
radiium <remip.dev@gmail.com>
2024-01-06 00:34:37 +0100

    fix phosphore svelte optimization

--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,7 @@ import adapter from '@sveltejs/adapter-vercel';
 import sveltePreprocess from 'svelte-preprocess';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
+import { phosphorSvelteOptimize } from "phosphor-svelte/preprocessor"
 
 const path = fileURLToPath(new URL('package.json', import.meta.url));
 const pkg = JSON.parse(readFileSync(path, 'utf8'));
@@ -12,11 +13,11 @@ const pkg = JSON.parse(readFileSync(path, 'utf8'));
 const config = {
     // Consult https://kit.svelte.dev/docs/integrations#preprocessors
     // for more information about preprocessors
-    preprocess: sveltePreprocess({
+    preprocess: [phosphorSvelteOptimize(), sveltePreprocess({
         scss: {
             prependData: "@use './src/lib/scss/mixins.scss' as *;"
         },
-    }),
+    })],
     kit: {
         // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
         // If your environment is not supported or you settled on a specific environment, switch out the adapter.
