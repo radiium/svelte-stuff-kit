d549473b7759ba21cd90db3621f545e64ce95de9
radiium <remip.dev@gmail.com>
2024-03-04 21:53:40 +0100

    doc: add mdsvex

--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,5 @@
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig from './mdsvex.config.js';
 // import adapter from '@sveltejs/adapter-auto';
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-vercel';
@@ -11,34 +13,37 @@ const pkg = JSON.parse(readFileSync(path, 'utf8'));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-    // for more information about preprocessors
-    preprocess: [
-        phosphorSvelteOptimize(),
-        sveltePreprocess({
-            scss: {
-                prependData: "@use './src/lib/scss/mixins.scss' as *;"
-            }
+	extensions: ['.svelte', ...mdsvexConfig.extensions],
+
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [
+		phosphorSvelteOptimize(),
+		sveltePreprocess({
+			scss: {
+				prependData: "@use './src/lib/scss/mixins.scss' as *;"
+			}
 		}),
 		mdsvex(mdsvexConfig)
-    ],
+	],
 
-    kit: {
-        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-        adapter: adapter(),
-        version: {
-            name: pkg.version
-        }
-    },
-    vitePlugin: {
-        inspector: {
-            toggleKeyCombo: 'meta-shift',
-            showToggleButton: 'always',
-            toggleButtonPos: 'bottom-right'
-        }
-    }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter: adapter(),
+		version: {
+			name: pkg.version
+		}
+	},
+
+	vitePlugin: {
+		inspector: {
+			toggleKeyCombo: 'meta-shift',
+			showToggleButton: 'always',
+			toggleButtonPos: 'bottom-right'
+		}
+	}
 };
 
 export default config;
