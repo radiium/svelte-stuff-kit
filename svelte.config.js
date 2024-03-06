f5d1faeae411f07c7577988b0110b31d913be05c
radiium <remip.dev@gmail.com>
2024-03-07 00:36:44 +0100

    fix: config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-vercel';
 import sveltePreprocess from 'svelte-preprocess';
+import { mdsvex } from 'mdsvex';
+import mdsvexConfig, { mdsvexExtensions } from './mdsvex.config.js';
 import { phosphorSvelteOptimize } from 'phosphor-svelte/preprocessor';
 import { readFileSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
@@ -11,37 +13,37 @@ const pkg = JSON.parse(readFileSync(path, 'utf8'));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', ...mdsvexConfig.extensions],
+    extensions: ['.svelte', ...mdsvexExtensions],
 
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [
-		phosphorSvelteOptimize(),
-		sveltePreprocess({
-			scss: {
-				prependData: "@use './src/lib/scss/mixins.scss' as *;"
-			}
-		}),
-		mdsvex(mdsvexConfig)
-	],
+    // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+    // for more information about preprocessors
+    preprocess: [
+        phosphorSvelteOptimize(),
+        sveltePreprocess({
+            scss: {
+                prependData: "@use './src/lib/scss/mixins.scss' as *;"
+            }
+        }),
+        mdsvex(mdsvexConfig)
+    ],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter(),
-		version: {
-			name: pkg.version
-		}
-	},
+    kit: {
+        // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+        // If your environment is not supported or you settled on a specific environment, switch out the adapter.
+        // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+        adapter: adapter(),
+        version: {
+            name: pkg.version
+        }
+    },
 
-	vitePlugin: {
-		inspector: {
-			toggleKeyCombo: 'meta-shift',
-			showToggleButton: 'always',
-			toggleButtonPos: 'bottom-right'
-		}
-	}
+    vitePlugin: {
+        inspector: {
+            toggleKeyCombo: 'meta-shift',
+            showToggleButton: 'always',
+            toggleButtonPos: 'bottom-right'
+        }
+    }
 };
 
 export default config;
