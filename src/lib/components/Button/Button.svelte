5fdb14fecb1d251eb4a27de769847443a4ed5267
radiium <remip.dev@gmail.com>
2024-03-12 07:47:40 +0100

    fix: button & buttonGroup

--- src/lib/components/Button/Button.svelte
+++ src/lib/components/Button/Button.svelte
@@ -1,255 +1,260 @@
 <script lang="ts">
-	import { getContext } from 'svelte';
-	import { clsx } from '../../utils/clsx';
-	import { ButtonGroupContextKey, defaultButtonProps } from './Button.props';
-	import type { ButtonProps } from './Button.types';
+    import { getContext } from 'svelte';
+    import { clsx } from '../../utils/clsx';
+    import { ButtonGroupContextKey, defaultButtonProps } from './Button.props';
+    import type { ButtonProps } from './Button.types';
 
-	type $$Props = ButtonProps;
-	export let elementRef: $$Props['elementRef'] = undefined;
-	export let size: $$Props['size'] = defaultButtonProps.size;
-	export let variant: $$Props['variant'] = defaultButtonProps.variant;
-	export let color: $$Props['color'] = defaultButtonProps.color;
-	export let align: $$Props['align'] = defaultButtonProps.align;
-	export let active: $$Props['active'] = defaultButtonProps.active;
-	export let iconOnly: $$Props['iconOnly'] = defaultButtonProps.iconOnly;
-	export let circle: $$Props['circle'] = defaultButtonProps.fullWidth;
-	export let fullWidth: $$Props['fullWidth'] = defaultButtonProps.fullWidth;
+    type $$Props = ButtonProps;
+    export let elementRef: $$Props['elementRef'] = undefined;
+    export let size: $$Props['size'] = defaultButtonProps.size;
+    export let variant: $$Props['variant'] = defaultButtonProps.variant;
+    export let color: $$Props['color'] = defaultButtonProps.color;
+    export let align: $$Props['align'] = defaultButtonProps.align;
+    export let active: $$Props['active'] = defaultButtonProps.active;
+    export let iconOnly: $$Props['iconOnly'] = defaultButtonProps.iconOnly;
+    export let circle: $$Props['circle'] = defaultButtonProps.fullWidth;
+    export let fullWidth: $$Props['fullWidth'] = defaultButtonProps.fullWidth;
 
-	const isInGroup = getContext<boolean | undefined>(ButtonGroupContextKey);
+    const isInGroup = getContext<boolean | undefined>(ButtonGroupContextKey);
 
-	$: cssClass = clsx($$restProps.class, 'Button', {
-		[`Button-${variant}`]: variant,
-		[`Button-size-${size}`]: size,
-		[`Button-${color}`]: color,
-		[`Button-align-${align}`]: align,
-		'Button-is-in-group': isInGroup,
-		'Button-full-width': fullWidth,
-		'Button-active': active,
-		'Button-icon-only': iconOnly,
-		'Button-circle': circle
-	});
+    $: cssClass = clsx($$restProps.class, 'Button', {
+        [`Button-${variant}`]: variant,
+        [`Button-size-${size}`]: size,
+        [`Button-${color}`]: color,
+        [`Button-align-${align}`]: align,
+        'Button-is-in-group': isInGroup,
+        'Button-full-width': fullWidth,
+        'Button-active': active,
+        'Button-icon-only': iconOnly,
+        'Button-circle': iconOnly && circle
+    });
 </script>
 
 <button
-	tabindex="0"
-	{...$$restProps}
-	data-color={color}
-	data-size={size}
-	class={cssClass}
-	style={$$restProps.style}
-	bind:this={elementRef}
-	on:click
-	on:submit
-	on:focus
-	on:blur
+    tabindex="0"
+    {...$$restProps}
+    data-color={color}
+    data-size={size}
+    class={cssClass}
+    style={$$restProps.style}
+    bind:this={elementRef}
+    on:click
+    on:submit
+    on:focus
+    on:blur
 >
-	<slot />
+    <slot />
 </button>
 
 <style lang="scss">
-	button.Button {
-		// CSS Vars
-		--button-min-width: calc(var(--space-5) * 2);
-		--button-width: unset;
-		--button-height: var(--space-5);
-		--button-padding: 0 var(--space-2);
-		--button-border: none;
-		--button-border-radius: var(--radius-3);
-		--button-background: transparent;
-		--button-background-hover: var(--accent-a6);
-		--button-background-active: var(--accent-a7);
-		--button-color: var(--accent-a12);
-		--button-font-size: var(--font-size-1);
-		--button-line-height: var(--line-height-1);
-		--button-letter-spacing: var(--letter-spacing-1);
-		--button-gap: var(--space-1);
-		--button-icon-height: 60%;
+    button.Button {
+        // CSS Vars
+        --button-min-width: calc(var(--space-5) * 2);
+        --button-width: unset;
+        --button-height: var(--space-5);
+        --button-padding: 0 var(--space-2);
+        --button-border: none;
+        --button-border-radius: var(--radius-3);
+        --button-background: transparent;
+        --button-background-hover: var(--accent-a6);
+        --button-background-active: var(--accent-a7);
+        --button-color: var(--accent-a12);
+        --button-font-size: var(--font-size-1);
+        --button-line-height: var(--line-height-1);
+        --button-letter-spacing: var(--letter-spacing-1);
+        --button-gap: var(--space-1);
+        --button-icon-height: 60%;
 
-		// Customizable
-		min-width: var(--button-min-width);
-		width: var(--button-width);
-		height: var(--button-height);
-		padding: var(--button-padding);
-		border: var(--button-border);
-		border-radius: var(--button-border-radius);
-		background-color: var(--button-background);
-		color: var(--button-color);
-		font-size: var(--button-font-size);
-		line-height: var(--button-line-height);
-		letter-spacing: var(--button-letter-spacing);
-		gap: var(--button-gap);
+        // Customizable
+        min-width: var(--button-min-width);
+        width: var(--button-width);
+        height: var(--button-height);
+        padding: var(--button-padding);
+        border: var(--button-border);
+        border-radius: var(--button-border-radius);
+        background-color: var(--button-background);
+        color: var(--button-color);
+        font-size: var(--button-font-size);
+        line-height: var(--button-line-height);
+        letter-spacing: var(--button-letter-spacing);
+        gap: var(--button-gap);
 
-		// Common
-		cursor: pointer;
-		position: relative;
-		box-sizing: border-box;
-		display: inline-flex;
-		flex-direction: row;
-		align-items: center;
-		flex-shrink: 0;
-		text-decoration: none;
-		white-space: nowrap;
-		user-select: none;
-		font-weight: bold;
-		transition: background-color linear 80ms;
+        // Common
+        cursor: pointer;
+        position: relative;
+        box-sizing: border-box;
+        display: inline-flex;
+        flex-direction: row;
+        align-items: center;
+        flex-shrink: 0;
+        text-decoration: none;
+        white-space: nowrap;
+        user-select: none;
+        font-weight: bold;
+        transition: background-color linear 80ms;
 
-		:global(svg) {
-			width: auto;
-			height: var(--button-icon-height);
-			fill: var(--button-color);
-		}
+        :global(svg) {
+            width: auto;
+            height: var(--button-icon-height);
+            fill: var(--button-color);
+        }
 
-		// In group
-		&.Button-is-in-group {
-			border-radius: 0;
+        // In group
+        &.Button-is-in-group {
+            border-radius: 0;
 
-			&:first-child {
-				border-top-left-radius: var(--button-border-radius);
-				border-bottom-left-radius: var(--button-border-radius);
-			}
-			&:last-child {
-				border-top-right-radius: var(--button-border-radius);
-				border-bottom-right-radius: var(--button-border-radius);
-			}
-		}
+            &:first-child {
+                border-top-left-radius: var(--button-border-radius);
+                border-bottom-left-radius: var(--button-border-radius);
+            }
+            &:last-child {
+                border-top-right-radius: var(--button-border-radius);
+                border-bottom-right-radius: var(--button-border-radius);
+            }
+        }
 
-		// Sizes
-		&.Button-size-1 {
-			--button-height: var(--space-5);
-			--button-min-width: calc(var(--space-5) * 2);
-			--button-padding: 0 var(--space-2);
-			--button-border-radius: var(--radius-3);
-			--button-gap: var(--space-1);
+        // Sizes
+        &.Button-size-1 {
+            --button-height: var(--space-5);
+            --button-min-width: calc(var(--space-5) * 2);
+            --button-padding: 0 var(--space-2);
+            --button-border-radius: var(--radius-3);
+            --button-gap: var(--space-1);
 
-			--button-font-size: var(--font-size-1);
-			--button-line-height: var(--line-height-1);
-			--button-letter-spacing: var(--letter-spacing-1);
+            --button-font-size: var(--font-size-1);
+            --button-line-height: var(--line-height-1);
+            --button-letter-spacing: var(--letter-spacing-1);
 
-			&.Button-icon-only {
-				--button-min-width: var(--space-5);
-				--button-width: var(--space-5);
-				--button-padding: var(--space-0);
-				--button-icon-height: 80%;
-			}
-		}
-		&.Button-size-2 {
-			--button-height: var(--space-6);
-			--button-min-width: calc(var(--space-6) * 2);
-			--button-padding: 0 var(--space-2);
-			--button-border-radius: var(--radius-3);
-			--button-gap: var(--space-1);
+            &.Button-icon-only {
+                --button-min-width: var(--space-5);
+                --button-width: var(--space-5);
+                --button-padding: var(--space-0);
+                --button-icon-height: 80%;
+            }
+        }
+        &.Button-size-2 {
+            --button-height: var(--space-6);
+            --button-min-width: calc(var(--space-6) * 2);
+            --button-padding: 0 var(--space-2);
+            --button-border-radius: var(--radius-3);
+            --button-gap: var(--space-1);
 
-			--button-font-size: var(--font-size-2);
-			--button-line-height: var(--line-height-2);
-			--button-letter-spacing: var(--letter-spacing-2);
+            --button-font-size: var(--font-size-2);
+            --button-line-height: var(--line-height-2);
+            --button-letter-spacing: var(--letter-spacing-2);
 
-			&.Button-icon-only {
-				--button-min-width: var(--space-6);
-				--button-width: var(--space-6);
-				--button-padding: var(--space-0);
-			}
-		}
-		&.Button-size-3 {
-			--button-height: var(--space-7);
-			--button-min-width: calc(var(--space-7) * 2);
-			--button-padding: 0 var(--space-3);
-			--button-border-radius: var(--radius-3);
-			--button-gap: var(--space-3);
+            &.Button-icon-only {
+                --button-min-width: var(--space-6);
+                --button-width: var(--space-6);
+                --button-padding: var(--space-0);
+            }
+        }
+        &.Button-size-3 {
+            --button-height: var(--space-7);
+            --button-min-width: calc(var(--space-7) * 2);
+            --button-padding: 0 var(--space-3);
+            --button-border-radius: var(--radius-3);
+            --button-gap: var(--space-3);
 
-			--button-font-size: var(--font-size-3);
-			--button-line-height: var(--line-height-3);
-			--button-letter-spacing: var(--letter-spacing-3);
+            --button-font-size: var(--font-size-3);
+            --button-line-height: var(--line-height-3);
+            --button-letter-spacing: var(--letter-spacing-3);
 
-			&.Button-icon-only {
-				--button-min-width: var(--space-7);
-				--button-width: var(--space-7);
-				--button-padding: var(--space-1);
-			}
-		}
-		&.Button-size-4 {
-			--button-height: var(--space-8);
-			--button-min-width: calc(var(--space-8) * 2);
-			--button-padding: 0 var(--space-4);
-			--button-border-radius: var(--radius-3);
-			--button-gap: var(--space-3);
+            &.Button-icon-only {
+                --button-min-width: var(--space-7);
+                --button-width: var(--space-7);
+                --button-padding: var(--space-1);
+            }
+        }
+        &.Button-size-4 {
+            --button-height: var(--space-8);
+            --button-min-width: calc(var(--space-8) * 2);
+            --button-padding: 0 var(--space-4);
+            --button-border-radius: var(--radius-3);
+            --button-gap: var(--space-3);
 
-			--button-font-size: var(--font-size-4);
-			--button-line-height: var(--line-height-4);
-			--button-letter-spacing: var(--letter-spacing-4);
+            --button-font-size: var(--font-size-4);
+            --button-line-height: var(--line-height-4);
+            --button-letter-spacing: var(--letter-spacing-4);
 
-			&.Button-icon-only {
-				--button-min-width: var(--space-8);
-				--button-width: var(--space-8);
-				--button-padding: var(--space-1);
-			}
-		}
+            &.Button-icon-only {
+                --button-min-width: var(--space-8);
+                --button-width: var(--space-8);
+                --button-padding: var(--space-1);
+            }
+        }
 
-		// Variants
-		&.Button-clear {
-			--button-border: none;
-			--button-color: var(--accent-a12);
-			--button-background: transparent;
-			--button-background-hover: var(--accent-a6);
-			--button-background-active: var(--accent-a7);
-		}
-		&.Button-outline {
-			--button-border: 1px solid var(--accent-9);
-			--button-color: var(--accent-a12);
-			--button-background: transparent;
-			--button-background-hover: var(--accent-a6);
-			--button-background-active: var(--accent-a7);
-		}
-		&.Button-soft {
-			--button-border: none;
-			--button-color: var(--accent-a12);
-			--button-background: var(--accent-a6);
-			--button-background-hover: var(--accent-a7);
-			--button-background-active: var(--accent-a8);
-		}
-		&.Button-solid {
-			--button-border: none;
-			--button-color: var(--contrast);
-			--button-background: var(--accent-9);
-			--button-background-hover: var(--accent-10);
-			--button-background-active: var(--accent-11);
-		}
+        // Variants
+        &.Button-clear {
+            --button-border: none;
+            --button-color: var(--accent-a12);
+            --button-background: transparent;
+            --button-background-hover: var(--accent-a6);
+            --button-background-active: var(--accent-a7);
+        }
+        &.Button-outline {
+            --button-border: 1px solid var(--accent-9);
+            --button-color: var(--accent-a12);
+            --button-background: transparent;
+            --button-background-hover: var(--accent-a6);
+            --button-background-active: var(--accent-a7);
+        }
+        &.Button-soft {
+            --button-border: none;
+            --button-color: var(--accent-a12);
+            --button-background: var(--accent-a6);
+            --button-background-hover: var(--accent-a7);
+            --button-background-active: var(--accent-a8);
+        }
+        &.Button-solid {
+            --button-border: none;
+            --button-color: var(--contrast);
+            --button-background: var(--accent-9);
+            --button-background-hover: var(--accent-10);
+            --button-background-active: var(--accent-11);
+        }
 
-		&.Button-icon-only {
-			&.Button-circle {
-				--button-border-radius: var(--radius-full);
-			}
-		}
+        &.Button-icon-only {
+            justify-content: center;
+            text-align: center;
+            
+            &.Button-circle {
+                --button-border-radius: var(--radius-full);
+            }
+        }
 
-		// Content
-		&.Button-align-start {
-			justify-content: flex-start;
-			text-align: start;
-		}
-		&.Button-align-center {
-			justify-content: center;
-			text-align: center;
-		}
-		&.Button-align-end {
-			justify-content: flex-end;
-			text-align: end;
-		}
-		&.Button-full-width {
-			width: 100%;
-		}
+        // Content
+        &:not(.Button-icon-only) {
+            &.Button-align-start {
+                justify-content: flex-start;
+                text-align: start;
+            }
+            &.Button-align-center {
+                justify-content: center;
+                text-align: center;
+            }
+            &.Button-align-end {
+                justify-content: flex-end;
+                text-align: end;
+            }
+            &.Button-full-width {
+                width: 100%;
+            }
+        }
 
-		// States
-		&:hover {
-			background: var(--button-background-hover);
-		}
-		&:active,
-		&.Button-active {
-			background: var(--button-background-active);
-		}
-		&:focus-visible {
-			@include input-box-shadow-focus;
-		}
-		&:disabled {
-			@include disabled;
-		}
-	}
+        // States
+        &:hover {
+            background: var(--button-background-hover);
+        }
+        &:active,
+        &.Button-active {
+            background: var(--button-background-active);
+        }
+        &:focus-visible {
+            @include input-box-shadow-focus;
+        }
+        &:disabled {
+            @include disabled;
+        }
+    }
 </style>
